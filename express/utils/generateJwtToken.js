383a5049814de8b8f1ed5b774b738fffbf547815
Md. Hasan Mia <hasanrafi69@gmail.com>
2024-03-15 01:26:17 +0600

    added express boilerplate

---
+++ express/utils/generateJwtToken.js
@@ -0,0 +1,12 @@
+const jwt = require("jsonwebtoken");
+const { JWT_EXPIRE, JWT_SECRET } = require("../constant");
+
+const sendToken = (user, statusCode, res) => {
+  const token = jwt.sign(user, JWT_SECRET, {
+    expiresIn: JWT_EXPIRE,
+  });
+
+  res.status(statusCode).json({ success: true, user, token });
+};
+
+module.exports = sendToken;
